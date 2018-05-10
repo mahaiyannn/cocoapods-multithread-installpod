f7f0b73de9028785f1d118299d8a37d794ec1fe0
Whirlwind <whirlwindjames@foxmail.com>
2018-05-10 17:52:28 +0800

    Add support for Pre-download

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in cocoapods-multithread-installpod.gemspec
 gemspec
-gem 'cocoapods', '1.0.0'
--- lib/cocoapods-multithread-installpod.rb
+++ lib/cocoapods-multithread-installpod.rb
@@ -1,121 +1,95 @@
 require "cocoapods-multithread-installpod/version"
-require 'thread'
+require 'thread/pool'
 
-if Pod::VERSION>='0.39.0'
-  module Pod
-    class Installer
-      def install_pod_sources
-        @installed_specs = []
-        pods_to_install = sandbox_state.added | sandbox_state.changed
-        title_options = { :verbose_prefix => '-> '.green }
+module Pod
+  class Installer
+    attr_accessor :worker
+    attr_accessor :todo_workers
+    attr_accessor :doing_workers
 
-        work_q = Queue.new
-        root_specs.sort_by(&:name).each{|spec| work_q.push spec }
-        workers = (0...20).map do
-          Thread.new do
-            begin
-              while spec = work_q.pop(true)
-                if pods_to_install.include?(spec.name)
-                  if sandbox_state.changed.include?(spec.name) && sandbox.manifest
-                    previous = sandbox.manifest.version(spec.name)
-                    title = "Installing #{spec.name} #{spec.version} (was #{previous})"
-                  else
-                    title = "Installing #{spec}"
-                  end
-                  UI.titled_section(title.green, title_options) do
-                    install_source_of_pod(spec.name)
-                    #UI.titled_section("Installed #{spec}", title_options)
-                  end
-                else
-                  UI.titled_section("Using #{spec}", title_options) do
-                    create_pod_installer(spec.name)
-                    #UI.titled_section("Installed #{spec}", title_options)
-                  end
-                end
-              end
-            rescue ThreadError
-            end
-          end
+    alias_method :ori_install_pod_sources, :install_pod_sources
+    def install_pod_sources
+      @worker = Thread.pool(5)
+      @todo_workers = []
+      @doing_workers = []
+      ori_install_pod_sources
+      @worker.shutdown
+      UI.titled_section "All download task completed!"
+    end
+
+    alias_method :ori_install_source_of_pod, :install_source_of_pod
+    def install_source_of_pod(pod_name)
+      @todo_workers << pod_name
+      @worker.process do
+        @todo_workers.delete(pod_name)
+        @doing_workers << pod_name
+        ori_install_source_of_pod(pod_name)
+        @doing_workers.delete(pod_name)
+        UI.titled_section "Download completed: #{pod_name} (Remain Doing: #{@doing_workers.count} Waiting: #{@todo_workers.count})"
+      end
+    end
+
+    class Analyzer
+      attr_accessor :worker
+      attr_accessor :todo_workers
+      attr_accessor :doing_workers
+
+      alias_method :ori_fetch_external_sources, :fetch_external_sources
+      def fetch_external_sources
+        @worker = Thread.pool(5)
+        @todo_workers = []
+        @doing_workers = []
+        ori_fetch_external_sources
+        @worker.shutdown
+        UI.titled_section "All download task completed!"
+      end
+
+      alias_method :ori_fetch_external_source, :fetch_external_source
+      def fetch_external_source(dependency, use_lockfile_options)
+        @todo_workers << dependency.name
+        @worker.process do
+          @todo_workers.delete(dependency.name)
+          @doing_workers << dependency.name
+          ori_fetch_external_source(dependency, use_lockfile_options)
+          @doing_workers.delete(dependency.name)
+          UI.titled_section "Download completed: #{dependency.name} (Remain Doing: #{@doing_workers.count} Waiting: #{@todo_workers.count})"
         end
-        UI.titled_section("Installing... Please Wait...", title_options)
-        workers.map(&:join)
       end
     end
 
 
-    module Downloader
-      # The class responsible for managing Pod downloads, transparently caching
-      # them in a cache directory.
-      #
-      class Cache
-        @@mutex=Mutex.new
-        def ensure_matching_version
-          @@mutex.lock
-          version_file = root + 'VERSION'
-          version = version_file.read.strip if version_file.file?
-
-          root.rmtree if version != Pod::VERSION && root.exist?
-          root.mkpath
-          version_file.open('w') { |f| f << Pod::VERSION }
-          @@mutex.unlock
+    class PodSourcePreparer
+      # Remove Dir.chidr to ensure thread safety.
+      alias_method :ori_run_prepare_command, :run_prepare_command
+      def run_prepare_command
+        return unless spec.prepare_command
+        UI.section(' > Running prepare command', '', 1) do
+          begin
+            ENV.delete('CDPATH')
+            ENV['COCOAPODS_VERSION'] = Pod::VERSION
+            prepare_command = spec.prepare_command.strip_heredoc.chomp
+            full_command = "cd \"#{path}\"\nset -e\n" + prepare_command
+            bash!('-c', full_command)
+          ensure
+            ENV.delete('COCOAPODS_VERSION')
+          end
         end
       end
     end
   end
 
-elsif Pod::VERSION=='0.35.0'
-  module Pod
-    class Installer
-      def install_pod_sources
-        @installed_specs = []
-        pods_to_install = sandbox_state.added | sandbox_state.changed
-        title_options = { :verbose_prefix => '-> '.green }
-        sorted_root_specs = root_specs.sort_by(&:name)
-        threads=[]
-        max_thread_count = 20.0
-        per_thead_task_count =(sorted_root_specs.count/max_thread_count).ceil
-        i = 0
-        while i < max_thread_count
-          sub_sorted_root_specs = sorted_root_specs[i*per_thead_task_count, per_thead_task_count]
-          if sub_sorted_root_specs != nil
-            threads << Thread.new(sub_sorted_root_specs) do |specs|
-              specs.each do |spec|
-                if pods_to_install.include?(spec.name)
-                  if sandbox_state.changed.include?(spec.name) && sandbox.manifest
-                    previous = sandbox.manifest.version(spec.name)
-                    title = "Installing #{spec.name} #{spec.version} (was #{previous})"
-                  else
-                    title = "Installing #{spec}"
-                  end
-                  UI.titled_section(title.green, title_options) do
-                    install_source_of_pod(spec.name)
-                  end
-                else
-                  UI.titled_section("Using #{spec}", title_options)
-                end
-              end
-            end
-          end
-          i+=1
-        end
-        threads.each{|t| t.join}
-      end
-    end
-
-    module UserInterface
-      class << self
-        def wrap_string(string, indent = 0)
-          if disable_wrap
-            string
-          else
-            first_space = ' ' * indent
-            # indented = CLAide::Helper.wrap_with_indent(string, indent, 9999)
-            # first_space + indented
-          end
-        end
+  module Downloader
+    # The class responsible for managing Pod downloads, transparently caching
+    # them in a cache directory.
+    #
+    class Cache
+      @@mutex=Mutex.new
+      alias_method :ori_ensure_matching_version, :ensure_matching_version
+      def ensure_matching_version
+        @@mutex.lock
+        ori_ensure_matching_version
+        @@mutex.unlock
       end
     end
   end
 end
-
-
